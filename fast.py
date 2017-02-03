f6a2a2b3e93478eca5e071de1541925116914255
afernandez7 <afernandez7@mail.usf.edu>
2017-02-03 15:02:39 -0500

    commented

--- fast.py
+++ fast.py
@@ -19,7 +19,7 @@ while True:
     # Capture frame-by-frame
     ret, img = cap.read()
 
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)    #sets image to grayscale
 
     faces = face_cascade.detectMultiScale(
         gray,
@@ -28,22 +28,22 @@ while True:
         )
 
     for (x, y, w, h) in faces:
-        cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)  #displays a rectangle around any detected faces
         detected = True
 
         roi_gray = gray[y+h/3:y+h, x:x+w]
         roi_color = img[y+h/3:y+h, x:x+w]
         
-        smiles = smile_cascade.detectMultiScale(
+        smiles = smile_cascade.detectMultiScale(                #detects smiles in image
             roi_gray,
             )
         
-        for (ex,ey,ew,eh) in smiles:
+        for (ex,ey,ew,eh) in smiles:                            #displays a rectangle around any detected smiles
             cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
             break
     
 
-    cv2.imshow('image', img)
+    cv2.imshow('image', img)                                    #feeds the rectangle layer to the colored picture
     
     k=cv2.waitKey(30) & 0xff
     if k == 27:
